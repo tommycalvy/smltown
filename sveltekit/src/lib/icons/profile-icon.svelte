542cf2b87cb9f10e888ab05617f99ee0056e74fa
Tommy Calvy <tommycalvy@Tommys-MacBook-Pro.local>
2023-02-15 23:09:54 -0600

    Started navbar with tailwind and daisyui

---
+++ sveltekit/src/lib/icons/profile-icon.svelte
@@ -0,0 +1,25 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	class={$$props.class}
+>
+	<defs />
+	<circle
+		id="shape0"
+		transform="translate(6.84, 1.8)"
+		r="5.16"
+		cx="5.16"
+		cy="5.16"
+		stroke-width="2"
+		stroke-linecap="square"
+		stroke-linejoin="bevel"
+	/><path
+		id="shape1"
+		transform="translate(2.5829596412556, 14.3497757847534)"
+		stroke-width="2"
+		stroke-linecap="square"
+		stroke-linejoin="bevel"
+		d="M19.1211 9.29148C18.7542 5.47521 18.3715 3.33471 17.9731 2.86996C17.5747 2.4052 14.7286 1.44855 9.43498 0C4.57381 1.38689 1.94302 2.33158 1.5426 2.83408C1.14218 3.33658 0.627981 5.45317 0 9.18386"
+	/>
+</svg>
--- sveltekit/src/routes/+layout.svelte
+++ sveltekit/src/routes/+layout.svelte
@@ -1,35 +1,67 @@
 <script>
+	import ProfileIcon from '$lib/icons/profile-icon.svelte';
 	import '../app.css';
+	
 </script>
 
-<div class="navbar bg-base-100">
-	<div class="flex-1">
-		<a class="btn btn-ghost normal-case text-xl">daisyUI</a>
-	</div>
-	<div class="flex-none gap-2">
-		<div class="form-control">
-			<input type="text" placeholder="Search" class="input input-bordered" />
-		</div>
-		<div class="dropdown dropdown-end">
-			<label tabindex="0" class="btn btn-ghost btn-circle avatar">
-				<div class="w-10 rounded-full">
-					<img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-				</div>
-			</label>
-			<ul
-				tabindex="0"
-				class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-			>
-				<li>
-					<a class="justify-between">
-						Profile
-						<span class="badge">New</span>
-					</a>
-				</li>
-				<li><a>Settings</a></li>
-				<li><a>Logout</a></li>
-			</ul>
-		</div>
-	</div>
+<svelte:head>
+	<title>SMLTOWN</title>
+</svelte:head>
+
+<input type="checkbox" id="modal-signup" class="modal-toggle" />
+<div class="modal modal-bottom sm:modal-middle">
+  <div class="modal-box">
+    <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div class="modal-action">
+      <label for="modal-signup" class="btn">Yay!</label>
+    </div>
+  </div>
 </div>
-<slot />
+
+<div class="bg-base-200 w-screen h-screen">
+	<nav class="navbar bg-base-100 min-h-12">
+		<div class="flex-1">
+			<a href="/" class="btn btn-ghost btn-sm normal-case text-xl">daisyUI</a>
+		</div>
+		<div class="flex-none gap-2">
+			
+			<div class="dropdown dropdown-bottom">
+				<input type="text" placeholder="Search" class="input input-sm input-bordered focus:border-primary hover:border-primary focus:outline-primary duration-75" />
+				<ul
+					
+					class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+				>
+					<li>
+						<a href="/profile" class="justify-between">
+							Profile
+							<span class="badge">New</span>
+						</a>
+					</li>
+					<li><a href="/settings">Settings</a></li>
+					<li><a href="/logout">Logout</a></li>
+				</ul>
+			</div>
+			<label for="modal-signup" class="btn btn-primary btn-sm normal-case">Sign Up</label>
+			<div class="dropdown dropdown-end">
+				<label tabindex="0" class="btn btn-ghost btn-square btn-sm avatar duration-150 outline-none active:bg-opacity-30">
+					<ProfileIcon class="inline-block w-6 h-6 stroke-current" />
+				</label>
+				<ul
+					tabindex="0"
+					class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+				>
+					<li>
+						<a href="/profile" class="justify-between">
+							Profile
+							<span class="badge">New</span>
+						</a>
+					</li>
+					<li><a href="/settings">Settings</a></li>
+					<li><a href="/logout">Logout</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<slot />
+</div>
